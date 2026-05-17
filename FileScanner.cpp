3e567dea442876e68ed68edf2eed70566573c78c
volodi4 <vladimir.svettsov@mail.ru>
2026-05-17 16:14:48 +0300

    first commit

---
+++ FileScanner.cpp
@@ -0,0 +1,19 @@
+//FileScanner.cpp
+
+#include <iostream>
+#include <string>
+#include <variant>
+#include <memory>
+#include <optional>
+#include <filesystem>
+#include <fstream>
+
+
+
+int main() {
+
+	std::cout << "Hello world!" << std::endl;
+
+
+	return 0;
+}
